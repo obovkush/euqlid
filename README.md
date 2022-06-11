d2266a79cf5c9e062e7378ed177e93995edcfc66
Oleg Bovkush <obovkush@gmail.com>
2022-06-11 10:56:07 +1000

    readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# obovkush.githab.io
-Hello!
+# Euqlid - сайт проектной организации.
+
+<p>&nbsp;</p>
+
+### Верстка сайта на HTML+CSS+JS+JQuery Pixel Perfect по макету Figma.
+
+<div>
+  <img src="https://github.com/devicons/devicon/blob/master/icons/javascript/javascript-original.svg" title="JavaScript" alt="JavaScript" width="40" height="40"/>&nbsp;
+  <img src="https://github.com/devicons/devicon/blob/master/icons/html5/html5-original.svg" title="HTML5" alt="HTML" width="40" height="40"/>&nbsp;
+  <img src="https://github.com/devicons/devicon/blob/master/icons/css3/css3-plain-wordmark.svg"  title="CSS3" alt="CSS" width="40" height="40"/>&nbsp;
+  <img src="https://github.com/devicons/devicon/blob/master/icons/jquery/jquery-original-wordmark.svg"  title="CSS3" alt="CSS" width="40" height="40"/>&nbsp;
+  <img src="https://github.com/devicons/devicon/blob/master/icons/figma/figma-original.svg"  title="CSS3" alt="CSS" width="40" height="40"/>&nbsp;
+  <img src="https://github.com/devicons/devicon/blob/master/icons/git/git-original-wordmark.svg" title="Git" **alt="Git" width="40" height="40"/>
+</div>
+
+<p>&nbsp;</p>
+
+![Demo Euqlid](/readme-assets/euqlid.gif)
+
+### Поиграть можно по [ссылке](https://obovkush.github.io/XO)
--- script/script.js
+++ script/script.js
@@ -1,28 +1,29 @@
 window.addEventListener('DOMContentLoaded', function () {
   // Burger
   document.querySelector('#burger').addEventListener('click', function () {
-    document.querySelector('#menu').classList.toggle('is-active')
-    document.querySelector('#overlay').classList.toggle('is-active')
-  })
+    document.querySelector('#menu').classList.toggle('is-active');
+    document.querySelector('#overlay').classList.toggle('is-active');
+  });
   document.querySelector('#overlay').addEventListener('click', function () {
-    document.querySelector('#menu').classList.toggle('is-active')
-    document.querySelector('#overlay').classList.toggle('is-active')
-  })
+    document.querySelector('#menu').classList.toggle('is-active');
+    document.querySelector('#overlay').classList.toggle('is-active');
+  });
   var buttons = document.querySelectorAll('.header-burger__link'),
-  index, button;
+    index,
+    button;
 
-    for (index = 0; index < buttons.length; index++) {
-      button = buttons[index];
-      button.addEventListener('click', clickHandler);
-      // button.addEventListener('dblclick', doubleClickHandler);
-    }
+  for (index = 0; index < buttons.length; index++) {
+    button = buttons[index];
+    button.addEventListener('click', clickHandler);
+    // button.addEventListener('dblclick', doubleClickHandler);
+  }
 
-    function clickHandler(event) {
-      document.querySelector('#menu').classList.toggle('is-active')
-        document.querySelector('#overlay').classList.toggle('is-active')
-    }
+  function clickHandler(event) {
+    document.querySelector('#menu').classList.toggle('is-active');
+    document.querySelector('#overlay').classList.toggle('is-active');
+  }
 
-  // Swiper  
+  // Swiper
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
@@ -34,12 +35,6 @@ window.addEventListener('DOMContentLoaded', function () {
       type: 'bullets',
       clickable: true,
     },
-
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
 
     // And if we need scrollbar
     scrollbar: {
@@ -54,24 +49,28 @@ window.addEventListener('DOMContentLoaded', function () {
   });
 
   // Slider
-  document.querySelectorAll('.section-work__nav-btn').forEach(function(workBtn) {
-    workBtn.addEventListener('click', function(event) {
-      const path = event.currentTarget.dataset.path
+  document
+    .querySelectorAll('.section-work__nav-btn')
+    .forEach(function (workBtn) {
+      workBtn.addEventListener('click', function (event) {
+        const path = event.currentTarget.dataset.path;
 
-      document.querySelectorAll('.section-work__info').forEach(function(workInfo) {
-        workInfo.classList.remove('section-work__info-active')
-      })
-      document.querySelector(`[data-target="${path}"]`).classList.add('section-work__info-active')
-    })
-  })
+        document
+          .querySelectorAll('.section-work__info')
+          .forEach(function (workInfo) {
+            workInfo.classList.remove('section-work__info-active');
+          });
+        document
+          .querySelector(`[data-target="${path}"]`)
+          .classList.add('section-work__info-active');
+      });
+    });
 
   // Accordeon
-  $( function() {
-    $( "#accordion" ).accordion({
+  $(function () {
+    $('#accordion').accordion({
       collapsible: true,
       active: false,
     });
-  } );
-
-})
-
+  });
+});
